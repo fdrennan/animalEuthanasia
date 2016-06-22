9347a0bb044465ad77fa68945f7805976ecd7e7e
Freddy Drennan <drennanfreddy@gmail.com>
2016-06-22 14:33:36 -0500

    First Commit

---
+++ ShelterAnimals.R
@@ -0,0 +1,13 @@
+# Import data
+
+train <- read.csv("train.csv", stringsAsFactors = FALSE, sep = ",")
+
+# Look at the structure/variables of the data
+
+str(train)
+
+# Create a subset of euthanized dogs
+
+euthanized_pets <- subset(train, OutcomeType == "Euthanasia")
+
+
