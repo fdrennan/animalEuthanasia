3726c8c2d1cb275d2e72ed729211768176be1489
Freddy Drennan <drennanfreddy@gmail.com>
2016-06-23 15:06:01 -0500

    Minor Updates

--- sortingFunction.R
+++ sortingFunction.R
@@ -10,12 +10,29 @@ sortit <- function(dataFrame = "Where data is stored", columnNumber = "Column nu
   
   unsortedDataFrame <- subsetOfDataFrame[,columnNumbersBeSorted]
   
+  # The code below is not yet necessary, but might be here soon. 
+  # residualDataFrame <- subsetOfDataFrame[,-columnNumbersBeSorted]
+  
+  # Make a simple counter and initialize a dataframe
+  i <- 1
+  frequencyTable <- NULL
+  
+  while (i <= length(columnNumbersBeSorted)) {
+        
+    frequencyTable[,i] <- as.data.frame(table(unsortedDataFrame[,i]))
+    
+    i <- i + 1
+  }
+  
+  return(frequencyTable)
   
   
 }
 
 # Running sortit to test results
 
-stuff <- sortit(train, 4, "Euthanasia", c(5,6,7,8,9,10))
+stuff <- sortit(train, 4, "Euthanasia", c(5,6,7,8,9))
 
 head(stuff)
+
+
